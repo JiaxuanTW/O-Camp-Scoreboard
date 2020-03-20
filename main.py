24a0ca243ee515ae67003c38834690e0fe82c76b
JiaxuanTW <48525471+JiaxuanTW@users.noreply.github.com>
2020-03-20 17:59:42 +0800

    master

--- main.py
+++ main.py
@@ -14,5 +14,37 @@ db = SQLAlchemy(app)
 def home():
     return render_template('index.html')
 
+@app.route('/team')
+def team():
+    return 'team'
+
+@app.route('/rank')
+def rank():
+    return 'rank'
+
+@app.route('/info/roommate')
+def roommate():
+    return 'roommate'
+
+@app.route('/info/teammate')
+def teammate():
+    return 'teammate'
+
+@app.route('/info/timetable')
+def timetable():
+    return 'timetable'
+
+@app.route('/bulletinboard')
+def bulletin_board():
+    return 'bulletin board'
+
+@app.route('/chat')
+def chat():
+    return 'chat'
+
+@app.route('/thanks')
+def thanks():
+    return 'thanks'
+
 if __name__ == '__main__':
     app.run(debug=True)
