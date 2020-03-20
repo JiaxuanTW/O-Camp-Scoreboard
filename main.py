3794a02deccdde415f22805f35feae2d007cd19e
JiaxuanTW <48525471+JiaxuanTW@users.noreply.github.com>
2020-03-20 17:40:51 +0800

    3/20 Create Database

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+p_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(p_dir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return 'This is my first web app.'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
